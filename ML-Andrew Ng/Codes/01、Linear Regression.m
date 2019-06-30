a1d0b3e1e5535cbaad8560b0c03d57d4f0117a32
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-06-30 14:38:21 +0800

    Week02CODE

--- ML-Andrew Ng/Codes/01、Linear Regression.m
+++ ML-Andrew Ng/Codes/01、Linear Regression.m
@@ -1,9 +1,9 @@
-%%% For one variable
-%-----------------------------------------------------------------------%
+% For one variable
+% ============================================================ %
 % WARMUPEXERCISE
 A = eye(5);
 
-% PLOTDATA function
+% PLOTDATA
 plot(x,y, 'rx', 'MarkerSize', 10);
 xlabel('population');
 ylabel('revenue');
@@ -13,7 +13,24 @@ J = sum((X*theta - y).^2)/2/m;                          % hint : write the dimen
 
 % GRADIENTDESCENT
 theta = theta - alpha/m*((X*theta - y)'*X)';
+% ============================================================ %
 
 
-%%% For multiple variables
-%-----------------------------------------------------------------------%
+% For multiple variables
+% ============================================================ %
+% COMPUTECOSTMULTI
+J = sum((X*theta - y).^2)/2/m;
+
+% GRADIENTDESCENTMULTI
+theta = theta - alpha/m*((X*theta - y)'*X)';            % same as one variable, has versatility
+
+% NORMALEQN
+theta = inv(X'*X)*X'*y;                                 % same as one variable, has versatility
+
+% FEATURENORMALIZE
+mu = mean(X);
+sigma = std(X);
+for i = 1:length(X(1,:))
+    X_norm(:,i) = (X_norm(:,i) - mu(i))/sigma(i);       % For feature normalization, your code should work for a dataset with any number of examples and features
+end                                                     % That is, X can be a matrix of any size
+% ============================================================ %
