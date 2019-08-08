a8841105421c31f87a7a53842e2139b154ac1616
Yi Tse Wu <44743933+tailer954@users.noreply.github.com>
2019-08-08 21:34:40 +0800

    Chapter06CODE

--- PYTHON-Charles Severance/Python Data Structures/Chapter 06、Strings.py
+++ PYTHON-Charles Severance/Python Data Structures/Chapter 06、Strings.py
@@ -1,29 +1,5 @@
-# 7.1
+# 6.5
 #=======================================================================
-fname = input("Enter file name: ")
-fh = open(fname)
-for line in fh:
-    line = line.strip()
-    line = line.upper()
-    print(line)
-    
-# 7.2
-#=======================================================================
-fname = input("Enter file name: ")
-fh = open(fname)
-Num = 0
-count = 0
-
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:") : 
-        continue
-    lineAns = line.strip()
-    lineAns = lineAns[20:]
-    lineAns = float(lineAns)
-    #print(lineAns)
-    Num = lineAns + Num
-    count = count + 1
-    
-ans = Num / count
-print("Average spam confidence:", ans)
-    
+text = "X-DSPAM-Confidence:    0.8475";
+index = text.find('0')
+print(float(text[index:index+6]))
