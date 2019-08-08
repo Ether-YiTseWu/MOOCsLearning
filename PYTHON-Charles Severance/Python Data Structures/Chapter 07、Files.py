b7207effceb92e4b012734e73512f368c9df3570
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-08-08 21:29:44 +0800

    Chapter07CODE

---
+++ PYTHON-Charles Severance/Python Data Structures/Chapter 07、Files.py
@@ -0,0 +1,29 @@
+# 7.1
+#=======================================================================
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    line = line.strip()
+    line = line.upper()
+    print(line)
+    
+# 7.2
+#=======================================================================
+fname = input("Enter file name: ")
+fh = open(fname)
+Num = 0
+count = 0
+
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : 
+        continue
+    lineAns = line.strip()
+    lineAns = lineAns[20:]
+    lineAns = float(lineAns)
+    #print(lineAns)
+    Num = lineAns + Num
+    count = count + 1
+    
+ans = Num / count
+print("Average spam confidence:", ans)
+    
