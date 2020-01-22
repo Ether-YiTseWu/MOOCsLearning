4d5724b8732cff0fff63e0b71d5586a082b2f5a4
Yi Tse Wu <44743933+tailer954@users.noreply.github.com>
2020-01-22 18:52:17 +0800

    Chapter13CODE_02

--- PYTHON-Charles Severance/Using Python to Access Web Data/Chapter 13、Programs that Surf the Web_02.py
+++ PYTHON-Charles Severance/Using Python to Access Web Data/Chapter 13、Programs that Surf the Web_02.py
@@ -8,10 +8,8 @@ Created on Wed Jan 22 12:46:28 2020
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
-for i in range(7):
-    r = urllib.request.urlopen('http://py4e-data.dr-chuck.net/known_by_Fikret.html')
+r = urllib.request.urlopen('http://py4e-data.dr-chuck.net/known_by_Fikret.html')
 htmlSoup = BeautifulSoup(r)
 
-nameList = list()
 for i in htmlSoup('a'):
     print(i.text)
